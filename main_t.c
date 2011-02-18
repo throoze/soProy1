23ce460ea2acd3850acca7b420bda265468414f4
Victor De Ponte <throoze@gmail.com>
2011-02-18 01:51:05 -0430

    Terminadas funciones de procesamiento de argumentos de entrada, genéricos tanto para la versión de procesos como de hilos...

--- main_t.c
+++ main_t.c
@@ -32,7 +32,7 @@ void job(){
 
 }
 
-void procesarNJobs(int argc, int *argv, int *nJobs, int *printFlag){
+void procesarNJobs(char *argv, int *nJobs){
   *nJobs = atoi(argv);
   if (*nJobs <= 0) {
     int opt;
@@ -64,23 +64,73 @@ void procesarNJobs(int argc, int *argv, int *nJobs, int *printFlag){
   }
 }
 
-void procesarArgumentos(int argc, int **argv, int *nJobs, int *printFlag) {
-  if (argc == 1) {
-    *nJobs = 8;
-    *printFlag = FALSE;    
-  } else {
+void procesarFlagPrint(char *argv, int *flagPrint){
+  *flagPrint = atoi(argv);
+  if (*flagPrint < 0 || 1 < *flagPrint) {
+    int opt;
+    printf("Los posibles valores para la opción de impresión son cero(0) o uno (1)!\n");
+    printf(USO);
+    do {
+      printf("\nQue desea hacer???\n");
+      printf("\t1) Volver a introducir la opción de impresión.\n");
+      printf("\t2) Usar el valor por defecto (0, no imprimir).\n");
+      printf("\t3) Abandonar el programa.\n");
+      printf("\n\tIntroduzca el numero de opción:\n\t\t>> ");
+      scanf("%d",&opt);
+    } while (1 > opt || opt >= 4);
+    switch (opt)
+      {
+      case 1:
+	do {
+	  printf("Por favor intruduzca un valor válido de opción de impresión:\n\t>> ");
+	  scanf("%d",flagPrint);
+	} while (*flagPrint != 0 || *flagPrint != 1);
+	break;
+      case 2:
+	*flagPrint = 0;
+	break;
+      case 3:
+	printf("Saliendo del programa...\n");
+	exit(0);
+      }
+  }
+}
+
+void procesarArgumentos(int argc, char **argv, int *nJobs, int *flagPrint) {
+  if (argc != 1) {
     switch (argc)
       {
       case 3:
 	if (strcmp(argv[1],"-n") == 0){
-	  procesarNJobs(argc,argv[2],nJobs,printFlag);
+	  procesarNJobs(argv[2],nJobs);
 	} else if (strcmp(argv[1],"-i") == 0) {
+	  procesarFlagPrint(argv[2],flagPrint);
 	} else {
 	  printf(USO);
 	  exit(1);
 	}
 	break;
       case 5:
+	if (strcmp(argv[1],"-n") == 0){
+	  if (strcmp(argv[3],"-i") == 0) {
+	    procesarNJobs(argv[2],nJobs);
+	    procesarFlagPrint(argv[4],flagPrint);
+	  } else {
+	    printf(USO);
+	    exit(1);
+	  }
+	} else if (strcmp(argv[1],"-i") == 0) {
+	  if (strcmp(argv[3],"-n") == 0) {
+	    procesarNJobs(argv[4],nJobs);
+	    procesarFlagPrint(argv[2],flagPrint);
+	  } else {
+	    printf(USO);
+	    exit(1);
+	  }
+	} else {
+	  printf(USO);
+	  exit(1);
+	}
 	break;
       default:
 	printf(USO);
@@ -90,11 +140,11 @@ void procesarArgumentos(int argc, int **argv, int *nJobs, int *printFlag) {
 }
 
 void main(int argc, char **argv){
-  int nJobs;
-  int printFlag;
+  int nJobs = 8;
+  int flagPrint = 0;
   printf("argc == %d\n\n", argc);
   
-  procesarArgumentos(argc, argv, &nJobs, &printFlag);
+  procesarArgumentos(argc, argv, &nJobs, &flagPrint);
   
-  printf("nJobs: %d, printFlag: %d\n\n", nJobs, printFlag);
+  printf("nJobs: %d, flagPrint: %d\n\n", nJobs, flagPrint);
 }
