1364e82019ea72b67fdda1a3c38576e2835a13ea
Victor De Ponte <throoze@gmail.com>
2011-02-19 15:00:21 -0430

    Versión de procesos bastante adelantada... falta procesar los datos recibidos de los hijos.

--- main_p.c
+++ main_p.c
@@ -21,123 +21,19 @@
 #include "almacenamiento.h"
 #endif
 
-#ifndef USETHREADS
-#define USETHREADS
-#include <pthread.h>
+#ifndef COMMON
+#define COMMON
+#include "reinascommon.h"
 #endif
 
-#define USO "\nreinas_t: Resuelve el problema de las 8 reinas usando hilos POSIX.\n\nDesarrollado por:\n\tVictor De Ponte, Carnet 05-38087\n\tIsaac Lopez, Carnet 07-41120\n\nUSO:\n\n\tSintaxis:\n\n\t\treinas_t [-n <num_trabajadores>] [-i {1|0}]\n\n\tOpciones:\n\n\t\t-n\tNumero de hilos a resolver el problema. El valor por\n\t\t\tdefecto es 8.\n\t\t-i\tOpcion de impresión. Debe ir seguido de un 1 si se desea\n\t\t\tque cada hilo imprima su resultado, o 0 en caso contra-\n\t\t\trio. El valor por defecto es 0.\n\n"
+#ifndef USEPROC
+#define USEPROC
+#include <unistd.h>
+#endif
 
-void job(){
-
-}
-
-void procesarNJobs(char *argv, int *nJobs){
-  *nJobs = atoi(argv);
-  if (*nJobs <= 0) {
-    int opt;
-    printf("Debe haber al menos un trabajador!");
-    printf(USO);
-    do {
-      printf("\nQue desea hacer???\n");
-      printf("\t1) Introducir un nuevo numero de trabajadores.\n");
-      printf("\t2) Usar el valor por defecto (8 trabajadores).\n");
-      printf("\t3) Abandonar el programa.\n");
-      printf("\n\tIntroduzca el numero de opción:\n\t\t>> ");
-      scanf("%d",&opt);
-    } while (1 > opt || opt >= 4);
-    switch (opt)
-      {
-      case 1:
-	do {
-	  printf("Por favor intruduzca un numero válido de trabajadores:\n\t>> ");
-	  scanf("%d",nJobs);
-	} while (*nJobs < 1);
-	break;
-      case 2:
-	*nJobs = 8;
-	break;
-      case 3:
-	printf("Saliendo del programa...\n");
-	exit(0);
-      }
-  }
-}
-
-void procesarFlagPrint(char *argv, int *flagPrint){
-  *flagPrint = atoi(argv);
-  if (*flagPrint < 0 || 1 < *flagPrint) {
-    int opt;
-    printf("Los posibles valores para la opción de impresión son cero(0) o uno (1)!\n");
-    printf(USO);
-    do {
-      printf("\nQue desea hacer???\n");
-      printf("\t1) Volver a introducir la opción de impresión.\n");
-      printf("\t2) Usar el valor por defecto (0, no imprimir).\n");
-      printf("\t3) Abandonar el programa.\n");
-      printf("\n\tIntroduzca el numero de opción:\n\t\t>> ");
-      scanf("%d",&opt);
-    } while (1 > opt || opt >= 4);
-    switch (opt)
-      {
-      case 1:
-	do {
-	  printf("Por favor intruduzca un valor válido de opción de impresión:\n\t>> ");
-	  scanf("%d",flagPrint);
-	} while (*flagPrint != 0 || *flagPrint != 1);
-	break;
-      case 2:
-	*flagPrint = 0;
-	break;
-      case 3:
-	printf("Saliendo del programa...\n");
-	exit(0);
-      }
-  }
-}
-
-void procesarArgumentos(int argc, char **argv, int *nJobs, int *flagPrint) {
-  if (argc != 1) {
-    switch (argc)
-      {
-      case 3:
-	if (strcmp(argv[1],"-n") == 0){
-	  procesarNJobs(argv[2],nJobs);
-	} else if (strcmp(argv[1],"-i") == 0) {
-	  procesarFlagPrint(argv[2],flagPrint);
-	} else {
-	  printf(USO);
-	  exit(1);
-	}
-	break;
-      case 5:
-	if (strcmp(argv[1],"-n") == 0){
-	  if (strcmp(argv[3],"-i") == 0) {
-	    procesarNJobs(argv[2],nJobs);
-	    procesarFlagPrint(argv[4],flagPrint);
-	  } else {
-	    printf(USO);
-	    exit(1);
-	  }
-	} else if (strcmp(argv[1],"-i") == 0) {
-	  if (strcmp(argv[3],"-n") == 0) {
-	    procesarNJobs(argv[4],nJobs);
-	    procesarFlagPrint(argv[2],flagPrint);
-	  } else {
-	    printf(USO);
-	    exit(1);
-	  }
-	} else {
-	  printf(USO);
-	  exit(1);
-	}
-	break;
-      default:
-	printf(USO);
-	exit(1);
-      }
-  }
-}
+#ifndef USO
+#define USO "\nreinas_p: Resuelve el problema de las 8 reinas usando Procesos hijos.\n\nDesarrollado por:\n\tVictor De Ponte, Carnet 05-38087\n\tIsaac Lopez, Carnet 07-41120\n\nUSO:\n\n\tSintaxis:\n\n\t\treinas_p [-n <num_trabajadores>] [-i {1|0}]\n\n\tOpciones:\n\n\t\t-n\tNumero de hijos a resolver el problema. El valor por\n\t\t\tdefecto es 8.\n\t\t-i\tOpcion de impresión. Debe ir seguido de un 1 si se desea\n\t\t\tque cada hijo imprima su resultado, o 0 en caso contra-\n\t\t\trio. El valor por defecto es 0.\n\n"
+#endif
 
 void main(int argc, char **argv){
   int nJobs = 8;
--- main_p.h
+++ main_p.h
@@ -1,3 +1,34 @@
+/* Archivo: main_p.h
+ * Proceso maestro.
+ * Autores: Victor De Ponte
+ *          Isaac Lopez
+ */
+#ifndef STD
+#define STD
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
+#define TRUE 1
+#define FALSE 0
+#define TAMAX 10
+#endif
+
+#ifndef ALM
+#define ALM
+#include "almacenamiento.h"
+#endif
+
+#ifndef COMMON
+#define COMMON
+#include "reinascommon.h"
+#endif
+
+#ifndef USEPROC
+#define USEPROC
+#include <unistd.h>
+#endif
+
+#ifndef USO
+#define USO "\nreinas_p: Resuelve el problema de las 8 reinas usando Procesos hijos.\n\nDesarrollado por:\n\tVictor De Ponte, Carnet 05-38087\n\tIsaac Lopez, Carnet 07-41120\n\nUSO:\n\n\tSintaxis:\n\n\t\treinas_p [-n <num_trabajadores>] [-i {1|0}]\n\n\tOpciones:\n\n\t\t-n\tNumero de hijos a resolver el problema. El valor por\n\t\t\tdefecto es 8.\n\t\t-i\tOpcion de impresión. Debe ir seguido de un 1 si se desea\n\t\t\tque cada hijo imprima su resultado, o 0 en caso contra-\n\t\t\trio. El valor por defecto es 0.\n\n"
+#endif
