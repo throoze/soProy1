60af42658d6d2988ddb7453f8f4de5789825fc40
narval <isaaclpe@gmail.com>
2011-02-19 00:22:44 -0500

    anhadido procedimiento-hijo sin salida por archivo

---
+++ pReinas.h
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define FALSE 0
+#define TRUE 1
+
+
+int a[8]; // a[i]=TRUE indica que no hay una reina en la i-ésima columna
+
+int b[15]; // b[i]=TRUE indica que no hay una reina en la i-ésima
+// diagonal que va desde el extremo superior derecho
+// hacia el inferior izquierdo
+
+int c[15]; // c[i]=TRUE indica que no hay una reina en la i-ésima
+// diagonal que va desde el extremo izquierdo
+// superior hacia el inferior derecho
+
+int x[8]; // indica la posición de la reina en la columna i-ésima
+
+int ri;
+int rj;
+
+void try(int i);
