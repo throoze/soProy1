60cad451c70a07ff5323e4c2ff947b8bb1dcac6a
Victor De Ponte <throoze@gmail.com>
2011-02-20 14:08:31 -0430

    agregados algunos includes, comentarios, y empeze con la lectura de los hijos.

--- main_p.h
+++ main_p.h
@@ -1,8 +1,10 @@
 /* Archivo: main_p.h
  * Proceso maestro.
+ * HEADER
  * Autores: Victor De Ponte
  *          Isaac Lopez
  */
+
 #ifndef STD
 #define STD
 #include <stdio.h>
@@ -32,3 +34,17 @@
 #ifndef USO
 #define USO "\nreinas_p: Resuelve el problema de las 8 reinas usando Procesos hijos.\n\nDesarrollado por:\n\tVictor De Ponte, Carnet 05-38087\n\tIsaac Lopez, Carnet 07-41120\n\nUSO:\n\n\tSintaxis:\n\n\t\treinas_p [-n <num_trabajadores>] [-i {1|0}]\n\n\tOpciones:\n\n\t\t-n\tNumero de hijos a resolver el problema. El valor por\n\t\t\tdefecto es 8.\n\t\t-i\tOpcion de impresión. Debe ir seguido de un 1 si se desea\n\t\t\tque cada hijo imprima su resultado, o 0 en caso contra-\n\t\t\trio. El valor por defecto es 0.\n\n"
 #endif
+
+/*----------------------------------------------------------------------------*/
+
+/*INICIO Funciones y procedimientos referentes al manejo de respuestas*/
+/**
+ * Se encarga de leer los archivos que construyen los hijos con las soluciones
+ * conseguidas.
+ * respuestas: Apuntador al Trie que almacenará las respuestas.
+ */
+void lectura(Trie *respuestas);
+
+/*FIN Funciones y procedimientos referentes al manejo de respuestas*/
+/*----------------------------------------------------------------------------*/
+/*FIN DEL ARCHIVO (EOF)*/
