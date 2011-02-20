34cc9120d54cef962c810ef3569dc0b122eb0276
Victor De Ponte <throoze@gmail.com>
2011-02-20 16:13:33 -0430

    Arreglado el sobreescrito del main_t.h

--- main_t.c
+++ main_t.c
@@ -6,7 +6,6 @@
 
 #include "main_t.h"
 #include "pReinas.h"
-
 #include "main_p.h"
 
 
@@ -38,10 +37,6 @@
 #ifndef USO
 #define USO "\nreinas_t: Resuelve el problema de las 8 reinas usando hilos POSIX.\n\nDesarrollado por:\n\tVictor De Ponte, Carnet 05-38087\n\tIsaac Lopez, Carnet 07-41120\n\nUSO:\n\n\tSintaxis:\n\n\t\treinas_t [-n <num_trabajadores>] [-i {1|0}]\n\n\tOpciones:\n\n\t\t-n\tNumero de hilos a resolver el problema. El valor por\n\t\t\tdefecto es 8.\n\t\t-i\tOpcion de impresión. Debe ir seguido de un 1 si se desea\n\t\t\tque cada hilo imprima su resultado, o 0 en caso contra-\n\t\t\trio. El valor por defecto es 0.\n\n"
 #endif
-
-void job(){
-
-}
 
 // ############# calculo reinas ####################
 
